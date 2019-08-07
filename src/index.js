174011c7277243114e51421d3d0eda6151ee040b
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2019-08-08 00:10:47 +0200

    fix: types and formatting

--- src/index.js
+++ src/index.js
@@ -1,34 +1,34 @@
-import { Header } from "./components/Header.js";
-import { Start } from "./pages/index.js";
+/// <reference path="../globals.d.ts" />
+
+import { Header } from './components/Header.js'
+import { Start } from './pages/index.js'
 
 export const state = {
-  currentPage: null
-};
-
-let header;
+  currentPage: null,
+}
 
 // Fonts
-let ocrFont;
+let ocrFont
 
 export function preload() {
-  ocrFont = loadFont("/src/assets/fonts/PressStart2P.ttf");
+  ocrFont = loadFont('/src/assets/fonts/PressStart2P.ttf')
 }
 
 export function setup() {
-  createCanvas(1155, 650);
+  createCanvas(1155, 650)
 
-  noStroke();
-  textFont(ocrFont);
+  noStroke()
+  textFont(ocrFont)
 
-  state.currentPage = new Start();
-  header = new Header();
+  state.currentPage = new Start()
+  header = new Header()
 }
 
 export function draw() {
-  background(0);
-  if (state.currentPage) state.currentPage.draw();
+  background(0)
+  if (state.currentPage) state.currentPage.draw()
 }
 
-window.keyPressed = function() {
-  if (state.currentPage.onKeyPress) state.currentPage.onKeyPress();
-};
+export function keyPressed() {
+  if (state.currentPage.onKeyPress) state.currentPage.onKeyPress()
+}
