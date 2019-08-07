63f7a1cd0ffe882a27659618631be4191ed6ba3e
riekehieke <30436310+riekehieke@users.noreply.github.com>
2019-08-08 00:26:55 +0200

    header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,14 @@ export class Header {
   home() {}
   draw() {
     noStroke()
-    fill(255)
+    fill(40)
     rect(0, 0, 1155, HEADER_HEIGHT)
-    fill(66, 135, 245)
-    textSize(20)
+    fill(255)
+    textSize(10)
     textAlign(LEFT, TOP)
-    text('ESC: HOME', 20, 15)
-    fill(243, 71, 255)
+    text('ESC TO HOME', 30, 20)
+    fill(255, 255, 0)
     textAlign(RIGHT, TOP)
-    text('Colormash', width - 15, 15)
+    text('COLOR MASH', width - 30, 20)
   }
 }
