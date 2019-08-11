afdeb60220d83b00a89faf3e6ef92d21eab7a393
Max <molukkenmax@Maxs-MBP.fritz.box>
2019-08-11 10:26:03 +0200

    texts updated

--- src/levels/index.js
+++ src/levels/index.js
@@ -2,8 +2,12 @@ import { createTilesFromText } from '../utils.js'
 import {
   transrapid,
   nineteenEightyFour,
-  abschied,
+  jobs,
   schiller,
+  africa,
+  kennedy,
+  reservoirDogs,
+  joker,
   monaLisa,
 } from './texts.js'
 
@@ -19,16 +23,40 @@ export const LEVEL_1984 = {
   tiles: createTilesFromText(nineteenEightyFour),
 }
 
-export const LEVEL_ABSCHIED = {
-  name: 'Abschied',
+export const LEVEL_JOBS = {
+  name: 'iPhone',
   mode: 'text',
-  tiles: createTilesFromText(abschied),
+  tiles: createTilesFromText(jobs),
 }
 
 export const LEVEL_AN_DIE_FREUDE = {
   name: 'Schiller',
   mode: 'text',
   tiles: createTilesFromText(schiller),
+}
+
+export const LEVEL_AFRICA = {
+  name: 'Africa',
+  mode: 'text',
+  tiles: createTilesFromText(africa),
+}
+
+export const LEVEL_KENNEDY = {
+  name: 'Kennedy',
+  mode: 'text',
+  tiles: createTilesFromText(kennedy),
+}
+
+export const LEVEL_RESERVOIR_DOGS = {
+  name: 'Reservoir\n Dogs',
+  mode: 'text',
+  tiles: createTilesFromText(reservoirDogs),
+}
+
+export const LEVEL_JOKER = {
+  name: 'Joker',
+  mode: 'text',
+  tiles: createTilesFromText(joker),
 }
 
 export const LEVEL_MONA_LISA = {
